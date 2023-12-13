385a097e48e3995ae52d43efe43426f18ccfaaba
JuanDavidCortesOssa <64558296+JuanDavidCortesOssa@users.noreply.github.com>
2023-12-13 04:31:18 -0500

    Gameplay adjustments

--- Assets/Scripts/Player/PlayerInteractions.cs
+++ Assets/Scripts/Player/PlayerInteractions.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInteractions : MonoBehaviour {
-    private void OnTriggerEnter2D(Collider2D other) {
+public class PlayerInteractions : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         if (!other.gameObject.CompareTag("Interactable")) return;
         other.gameObject.GetComponent<IInteractable>().Interact();
     }
